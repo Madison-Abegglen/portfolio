788815754c992bfd99c2582ed284b9acfc317558
Madison Abegglen <madison.abegglen@gmail.com>
2018-12-11 18:29:06 -0700

    first push

---
+++ src/components/navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <h1>This is the navbar</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navbar"
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container-fluid">
+    <navbar></navbar>
+    <div class="row">
+      <div class="col-12">
+        <h1>This is the home screen</h1>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import navbar from "@/components/navbar.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    navbar
   }
-}
+};
 </script>
